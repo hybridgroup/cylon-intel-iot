38aa81594e0359be9e8c6ce779cf6670ae80aeab
deadprogram <ron@hybridgroup.com>
2015-08-31 12:48:09 -0700

    Add entire list of suported upm drivers

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ var Adaptor = require("./lib/adaptor"),
 
 module.exports = {
   adaptors: ["intel-iot"],
-  drivers: ["upm-hmc5883l", "upm-lcm1602", "upm-jhd1313m1", "upm-grovetemp"],
+  drivers: ["upm-grovemoisture", "upm-groverelay"
+            "upm-grovespeaker", "upm-grovetemp",
+            "upm-grovewfs", "upm-adxl345",
+            "upm-at42qt1070", "upm-biss0001",
+            "upm-bmpx8x", "upm-hmc5883l",
+            "upm-jhd1313m1", "upm-lcm1602",
+            "upm-ldt0028", "upm-mma7660",
+            "upm-otp538u", "upm-rfr359f",
+            "upm-ssd1308", "upm-tp401",
+            "upm-tsl2561", "upm-yg1006"],
   dependencies: ["cylon-gpio", "cylon-i2c"],
 
   adaptor: function(opts) {
