9dd5feaeb62f85ba7cd5fef1ae9317063b375236
deadprogram <ron@hybridgroup.com>
2016-04-27 13:53:26 -0700

    Remove extra spaces from example
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- examples/upm_ppd42ns/upm_ppd42ns.js
+++ examples/upm_ppd42ns/upm_ppd42ns.js
@@ -16,7 +16,7 @@ Cylon.robot({
     every(1000, my.led.toggle);
     every(30000, function() {
       console.log("Reading dust sensor...");
-      
+
       var data = my.ppd42ns.getData();
       console.log("Low pulse occupancy: " + data.lowPulseOccupancy);
       console.log("Ratio: " + data.ratio);
